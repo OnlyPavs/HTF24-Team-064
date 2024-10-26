188d1ece457945ec5cf9eab9df677e90cbbc04e4
Sandy <psaisandeep2002@gmail.com>
2024-10-26 23:25:00 +0530

    webpage add

--- CryptoInsights/cryptoinsights/src/App.js
+++ CryptoInsights/cryptoinsights/src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Featured from './components/Featured'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Homepage from './components/Homepage'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HomePage Of Crypto Insights is loading
-        </p>
-        
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+      <Homepage />
+      <Footer />
+    </>
   );
 }
 
